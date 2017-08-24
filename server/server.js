ea814c2c86267a816eb0018d9a76d753e78a93fb
Yifan Sun <sunyifan112358@gmail.com>
2017-08-23 22:55:25 -0400

    Connect to database

--- server/server.js
+++ server/server.js
@@ -1,21 +1,50 @@
+'use strict';
 const fs = require('fs');
 const https = require('https');
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
-var privateKey = fs.readFileSync('/home/yifan/cert/server.key', 'utf8');
-var certificate = fs.readFileSync('/home/yifan/cert/server.crt', 'utf8');
+var connection = initDB()
+var app = initServer();
 
-var credentials = {key: privateKey, cert: certificate};
+function initDB() {
+    let connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : 'root',
+      database : 'game_logger'
+    });
+    
+    connection.connect();
+    return connection;
+}
 
-var app = express();
-var httpsServer = https.createServer(credentials, app);
-httpsServer.listen(18443);
+function initServer() {
+    let privateKey = fs.readFileSync('/home/yifan/cert/server.key', 'utf8');
+    let certificate = fs.readFileSync('/home/yifan/cert/server.crt', 'utf8');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+    let credentials = {key: privateKey, cert: certificate};
+
+    let app = express();
+    let httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(18443);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    return app;
+}
 
 app.get('/api/log', function(req, res) {
     console.log(req.query);
+
+    connection.query("SELECT * FROM game_logger.game_runs WHERE run_id='a'", function(error, result, fields) {
+        if (error) throw error;
+        console.log(result);
+    })
+
     res.send("");
 });
+
+console.log('Server ready');
